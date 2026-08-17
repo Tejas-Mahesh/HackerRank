2dab1e64123d134e359ccf496597003e85d56e89
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-17 20:14:55 +0530

    Create ifelifelse.py

---
+++ ifelifelse.py
@@ -0,0 +1,16 @@
+#!/bin/python
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 != 0:
+        print('Weird')
+    elif n>=2 and n<=5:
+        print("Not Weird")
+    elif n>=6 and n<=20:
+            print("Not Weird")
+    elif n>20:
+        print("Not Weird")
+            
+
+
+
+
