4792bd919c269304ae17b79be20309ad53c9e45f
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-20 18:13:03 +0530

    Create array_operation.py

---
+++ array_operation.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    for i in range(n-1,-1,-1):
+        print(f"{arr[i]}",end=' ')
+#another method
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(arr[::-1])
