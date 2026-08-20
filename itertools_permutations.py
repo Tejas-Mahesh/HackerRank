b2dfd744f7a3cbcb263fad89e7f12b4532503165
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-20 18:40:32 +0530

    Create itertools_permutations.py

---
+++ itertools_permutations.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+#does permutation of words useing itertools permutations
+#input exampel hack 2
+from itertools import permutations
+a=list(map(str,input().split()))
+result=list(permutations(a[0],int(a[1])))
+result.sort()
+for i in result:
+    print(''.join(i))
+
