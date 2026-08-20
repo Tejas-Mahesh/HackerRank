4bd34b72fa7936f270b4939e96abe250ac0f949f
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-20 20:05:43 +0530

    Create itertools_combinations.py

---
+++ itertools_combinations.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+
+#combinations by sorting str
+a = list(map(str, input().split()))
+
+b = a[0]
+ss=[]
+for strr in b:
+    ss.append(strr)
+ss.sort()
+''.join(ss)
+c = int(a[1])
+
+store = []
+
+for i in range(1, c + 1):
+    result = list(combinations(ss, i))
+    result.sort()
+
+    for item in result:
+        print(''.join(item))
