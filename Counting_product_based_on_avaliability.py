5c577942ec87bb9173c1365561c2f3b500313389
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-21 19:09:46 +0530

    Update Counting_product_based_on_avaliability.py

--- Counting_product_based_on_avaliability.py
+++ Counting_product_based_on_avaliability.py
@@ -19,5 +19,17 @@ for _ in range(customers):
         shoe_count[size] -= 1
 
 print(money)
+#input
+10
+2 3 4 5 6 8 7 6 5 18 #for this use Counter
+6
+6 55
+6 45
+6 55
+4 40
+18 60
+10 50
+#Expected Output
+200
             
      
