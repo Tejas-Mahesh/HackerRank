f81d1354889d4ada63c7d1d4357f1c7d122101d2
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-21 19:37:05 +0530

    Create Defaultdictuse.py

---
+++ Defaultdictuse.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+
+n, m = map(int, input().split())
+
+d = defaultdict(list)
+
+# Group A
+for i in range(1, n + 1):
+    word = input()
+    d[word].append(i)
+
+# Group B
+for _ in range(m):
+    word = input()
+
+    if word in d:
+        print(*d[word])
+    else:
+        print(-1)
