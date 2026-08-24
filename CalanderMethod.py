ee1345b46200b23cf7cae9050239b74636818330
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-24 21:15:19 +0530

    Create CalanderMethod.py

---
+++ CalanderMethod.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import calendar
+
+month, day, year = map(int, input().split())
+
+weekday = calendar.weekday(year, month, day)
+
+print(calendar.day_name[weekday].upper())
+
