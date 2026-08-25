0967001a050786680f4e72b528ac4eaa202f776a
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-25 21:05:13 +0530

    Update NonDuplicateValueAverage.py

--- NonDuplicateValueAverage.py
+++ NonDuplicateValueAverage.py
@@ -13,3 +13,8 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
+#input 
+10
+161 182 161 154 176 170 167 171 170 174
+#output
+169.375
