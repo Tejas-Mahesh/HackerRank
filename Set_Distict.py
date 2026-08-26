dfd3414016b758b9f7970c983867cbc33fe7e618
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-26 17:21:17 +0530

    Update Set_Distict.py

--- Set_Distict.py
+++ Set_Distict.py
@@ -4,3 +4,17 @@ for i in range(total_no):
     country=input()
     set1.add(country)
 print(len(set1))
+
+
+#Input (stdin)
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+#Expected Output
+5
+#the set take only the unique value only
