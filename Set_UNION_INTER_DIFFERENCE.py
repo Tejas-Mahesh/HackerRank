3028431c0cd11c8d8fad546df7884220f1049fa7
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-26 18:38:40 +0530

    Update Set_UNION_INTER_DIFFERENCE.py

--- Set_UNION_INTER_DIFFERENCE.py
+++ Set_UNION_INTER_DIFFERENCE.py
@@ -22,3 +22,11 @@ m=int(input())
 set2=set(map(int,input().split()))
 total=set1.intersection(set2)
 print(len(total))
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+set1=set(map(int,input().split()))
+m=int(input())
+set2=set(map(int,input().split()))
+total=set1.difference(set2)
+print(len(total))
