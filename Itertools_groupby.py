d63f30d9d4627b87344ce4cf781adc6e49daa47b
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-27 19:28:52 +0530

    Update Itertools_groupby.py

--- Itertools_groupby.py
+++ Itertools_groupby.py
@@ -5,3 +5,9 @@ s = input()
 
 for key, group in groupby(s):
     print((len(list(group)), int(key)), end=" ")
+i#nput (stdin)
+1222311
+#Your Output (stdout)
+(1, 1) (3, 2) (1, 3) (2, 1) 
+#Expected Output
+(1, 1) (3, 2) (1, 3) (2, 1)
