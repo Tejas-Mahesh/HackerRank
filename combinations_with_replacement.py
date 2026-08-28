c135357b45fe28e520efafeb57139fda9150eb29
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-28 21:03:34 +0530

    Create combinations_with_replacement.py

---
+++ combinations_with_replacement.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations_with_replacement
+
+s, k = input().split()
+
+s = sorted(s)
+
+for combination in combinations_with_replacement(s, int(k)):
+    print(''.join(combination))
+
