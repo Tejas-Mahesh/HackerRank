0a1cb709ff9f4d9cfa4a9fc715b3cb1bd4bbc1dc
TEJAS M <tejasmanjulakg@gmail.com>
2026-08-28 21:04:22 +0530

    Update combinations_with_replacement.py

--- combinations_with_replacement.py
+++ combinations_with_replacement.py
@@ -7,4 +7,16 @@ s = sorted(s)
 
 for combination in combinations_with_replacement(s, int(k)):
     print(''.join(combination))
-
+#input
+HACK 2
+#Expected Output
+AA
+AC
+AH
+AK
+CC
+CH
+CK
+HH
+HK
+KK
